f7c6ae43cc9791867f1cc866c29445de0c80b1ef
Manish Mathai <manishpmathai@gmail.com>
2020-07-13 03:21:24 -0700

    Hello triangle

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,33 @@
 extern crate gl;
 extern crate glfw;
 
+use gl::types::*;
 use glfw::{Action, Context, Glfw, InitError, Key, Window, WindowEvent};
-use std::process;
+use std::ffi::CString;
+use std::os::raw::c_void;
 use std::sync::mpsc::Receiver;
+use std::{mem, process, ptr};
 
 const INIT_WIDTH: u32 = 800;
 const INIT_HEIGHT: u32 = 600;
+
+const VERTEX_SHADER_SOURCE: &str = r#"
+#version 330 core
+layout (location = 0) in vec3 pos;
+
+void main() {
+    gl_Position = vec4(pos.x, pos.y, pos.z, 1.0f);
+}
+"#;
+
+const FRAGMENT_SHADER_SOURCE: &str = r#"
+#version 330 core
+out vec4 FragColor;
+
+void main() {
+    FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);
+}
+"#;
 
 fn configure_glfw() -> Result<Glfw, InitError> {
     match glfw::init(glfw::FAIL_ON_ERRORS) {
@@ -44,6 +65,126 @@ fn configure_gl(window: &mut Window) {
     gl::load_with(|symbol| window.get_proc_address(symbol) as *const _);
 }
 
+unsafe fn setup_program() -> GLuint {
+    let vertex_shader_obj = gl::CreateShader(gl::VERTEX_SHADER);
+    let vertex_shader_src = CString::new(VERTEX_SHADER_SOURCE.as_bytes()).unwrap();
+    gl::ShaderSource(
+        vertex_shader_obj,
+        1,
+        &vertex_shader_src.as_ptr(),
+        ptr::null(),
+    );
+    gl::CompileShader(vertex_shader_obj);
+    if let ShaderCompileStatus::FAILURE(err_msg) = get_shader_compile_status(vertex_shader_obj) {
+        eprintln!("Vertex shader compilation failed with error: {}", err_msg);
+        process::exit(1);
+    }
+
+    let fragment_shader_obj = gl::CreateShader(gl::FRAGMENT_SHADER);
+    let fragment_shader_src = CString::new(FRAGMENT_SHADER_SOURCE.as_bytes()).unwrap();
+    gl::ShaderSource(
+        fragment_shader_obj,
+        1,
+        &fragment_shader_src.as_ptr(),
+        ptr::null(),
+    );
+    gl::CompileShader(fragment_shader_obj);
+    if let ShaderCompileStatus::FAILURE(err_msg) = get_shader_compile_status(fragment_shader_obj) {
+        eprintln!("Fragment shader compilation failed with error: {}", err_msg);
+        process::exit(1);
+    }
+
+    let shader_program = gl::CreateProgram();
+    gl::AttachShader(shader_program, vertex_shader_obj);
+    gl::AttachShader(shader_program, fragment_shader_obj);
+    gl::LinkProgram(shader_program);
+
+    let mut link_success = gl::FALSE as GLint;
+    let mut link_log = Vec::with_capacity(512);
+    link_log.set_len(link_log.capacity() - 1);
+    gl::GetProgramiv(shader_program, gl::LINK_STATUS, &mut link_success);
+    if link_success != gl::TRUE as GLint {
+        gl::GetProgramInfoLog(
+            shader_program,
+            512,
+            ptr::null_mut(),
+            link_log.as_mut_ptr() as *mut GLchar,
+        );
+        eprintln!(
+            "Shader program linking failed with error: {}",
+            String::from_utf8(link_log).unwrap()
+        );
+        process::exit(1);
+    }
+
+    gl::DeleteShader(vertex_shader_obj);
+    gl::DeleteShader(fragment_shader_obj);
+
+    shader_program
+}
+
+enum ShaderCompileStatus {
+    SUCCESSFUL,
+    FAILURE(String),
+}
+
+unsafe fn get_shader_compile_status(shader_obj: GLuint) -> ShaderCompileStatus {
+    let mut compile_success = gl::FALSE as GLint;
+    let mut compile_log = Vec::with_capacity(512);
+    compile_log.set_len(compile_log.capacity() - 1);
+    gl::GetShaderiv(shader_obj, gl::COMPILE_STATUS, &mut compile_success);
+    if compile_success != gl::TRUE as GLint {
+        gl::GetShaderInfoLog(
+            shader_obj,
+            512,
+            ptr::null_mut(),
+            compile_log.as_mut_ptr() as *mut GLchar,
+        );
+        ShaderCompileStatus::FAILURE(String::from_utf8(compile_log).unwrap())
+    } else {
+        ShaderCompileStatus::SUCCESSFUL
+    }
+}
+
+fn setup_scene() -> (GLuint, GLuint) {
+    unsafe {
+        let shader_program = setup_program();
+
+        let vertices: [f32; 9] = [-0.5, -0.5, 0.0, 0.5, -0.5, 0.0, 0.0, 0.5, 0.0];
+
+        let (mut triangle_buffer_obj, mut triangle_array_obj) = (0, 0);
+        gl::GenVertexArrays(1, &mut triangle_array_obj);
+        gl::GenBuffers(1, &mut triangle_buffer_obj);
+
+        // Bind objects
+        gl::BindVertexArray(triangle_array_obj);
+        gl::BindBuffer(gl::ARRAY_BUFFER, triangle_buffer_obj);
+
+        // Setup buffer data and properties
+        gl::BufferData(
+            gl::ARRAY_BUFFER,
+            (vertices.len() * mem::size_of::<GLfloat>()) as GLsizeiptr,
+            &vertices[0] as *const f32 as *const c_void,
+            gl::STATIC_DRAW,
+        );
+        gl::VertexAttribPointer(
+            0,
+            3,
+            gl::FLOAT,
+            gl::FALSE,
+            3 * mem::size_of::<GLfloat>() as GLsizei,
+            ptr::null(),
+        );
+        gl::EnableVertexAttribArray(0);
+
+        // Unbind objects
+        gl::BindBuffer(gl::ARRAY_BUFFER, 0);
+        gl::BindVertexArray(0);
+
+        (shader_program, triangle_array_obj)
+    }
+}
+
 pub fn main() {
     let mut glfw_obj;
     let mut window;
@@ -58,7 +199,10 @@ pub fn main() {
                     events = result.1;
                     configure_gl(&mut window);
                 }
-                None => panic!("Exiting due to GLFW Window creation failure."),
+                None => {
+                    eprintln!("Exiting due to GLFW Window creation failure.");
+                    process::exit(1);
+                }
             }
         }
         Err(e) => {
@@ -66,6 +210,8 @@ pub fn main() {
             process::exit(1);
         }
     }
+
+    let (shader_program, scene_array_obj) = setup_scene();
 
     while !window.should_close() {
         // Process Events
@@ -75,6 +221,11 @@ pub fn main() {
         unsafe {
             gl::ClearColor(0.2, 0.3, 0.3, 1.0);
             gl::Clear(gl::COLOR_BUFFER_BIT);
+
+            gl::UseProgram(shader_program);
+            gl::BindVertexArray(scene_array_obj);
+            gl::DrawArrays(gl::TRIANGLES, 0, 3);
+            gl::BindVertexArray(0);
         }
 
         // Swap buffer and poll events
